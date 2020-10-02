574316de74c93c42e2195057340c1aacc18e10f6
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-10-03 04:48:00 +0500

    Add components and routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar";
+import exercisesList from "./components/exercisesList";
+import editExercise from "./components/editExercise";
+import createExercise from "./components/createExercise";
+import createUser from "./components/createUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={exercisesList} />
+        <Route path="/edit/:id" component={editExercise} />
+        <Route path="/create" component={createExercise} />
+        <Route path="/user" component={createUser} />
+      </div>
+    </Router>
   );
 }
 
