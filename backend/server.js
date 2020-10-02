e51333b311c46bd26abfccfd92c70f75214b708c
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-10-03 03:02:28 +0500

    Add exercise and user route in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -19,8 +22,11 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-app.use(cors());
-app.use(express.json());
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
